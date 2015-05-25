dec64bdbdba700281dca7b827351497939fa7890
slevigouroux@trapeze.com <slevigouroux@trapeze.com>
2015-05-25 13:22:00 -0400

    icon activity

---
+++ app/background.js
@@ -0,0 +1,31 @@
+﻿
+var enabled = false;
+
+function disableBrowserAction(){
+    chrome.browserAction.setIcon({path:"img/inactive.png"});
+}
+
+function enableBrowserAction(){
+    chrome.browserAction.setIcon({path:"img/active.png"});
+	
+	/*chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+	  chrome.tabs.sendMessage(tabs[0].id, {greeting: "hello"}, function(response) {
+		console.log(response.farewell);
+	  });
+	});*/
+
+
+}
+
+function updateState(){
+    if(enabled==false){
+        enabled=true;
+        enableBrowserAction();
+    }else{
+        enabled=false;
+        disableBrowserAction();
+    }
+}
+
+disableBrowserAction();
+chrome.browserAction.onClicked.addListener(updateState);
--- app/script.js
+++ app/script.js
@@ -1,28 +1,34 @@
-﻿
-var wikipedia = "http://en.wikipedia.org/wiki/Marketing";
+﻿var enabled = true;
 
-if (window.location.host != "en.wikipedia.org")
-{
-    if (document.getElementById("content") != null) {
-        window.location = wikipedia + '#' + window.location;
-    } else {
-        console.error('No id "content" in this html sorry, the extension can\'t turn this page into a wikipedia one.');
-    }
-}
-else
-{
-    var contentSiteUrl = window.location.hash.substring(1, window.location.hash.length);
-    var urlTemp = contentSiteUrl.replace('//', 'MARK');
-    var hostIndex = urlTemp.indexOf('/') + 1;
-    var host = urlTemp.substr(0, hostIndex).replace('MARK', '//');
-    console.log(host);
+var wikipedia1 = "http://en.wikipedia.org/wiki/Marketing";
+var wikipedia2 = "http://en.wikipedia.org/wiki/Canada";
 
-    doCORSRequest({ method: 'GET', url: contentSiteUrl, data: '' }, function printResult(rawHtml) {
-        rawHtml = rawHtml.replace(/href="\//g, 'href="' + host);
-        var $html = $('<div />', { html: rawHtml });
-        var htmlContent = $html.find('#content');
-        document.getElementById("content").innerHTML = htmlContent.html();
-    });
+
+if(enabled)
+{
+	if (window.location.host != "en.wikipedia.org")
+	{
+		if (document.getElementById("content") != null) {
+			window.location = wikipedia1 + '#' + window.location;
+		} else {
+			console.error('No id "content" in this html sorry, the extension can\'t turn this page into a wikipedia one.');
+		}
+	}
+	else
+	{
+		var contentSiteUrl = window.location.hash.substring(1, window.location.hash.length);
+		var urlTemp = contentSiteUrl.replace('//', 'MARK');
+		var hostIndex = urlTemp.indexOf('/') + 1;
+		var host = urlTemp.substr(0, hostIndex).replace('MARK', '//');
+		console.log(host);
+
+		doCORSRequest({ method: 'GET', url: contentSiteUrl, data: '' }, function printResult(rawHtml) {
+			rawHtml = rawHtml.replace(/href="\//g, 'href="' + wikipedia1 + '#' + host);
+			var $html = $('<div />', { html: rawHtml });
+			var htmlContent = $html.find('#content');
+			document.getElementById("content").innerHTML = htmlContent.html();
+		});
+	}
 }function doCORSRequest(options, printResult) {
     var x = new XMLHttpRequest();
     x.open(options.method, 'https://cors-anywhere.herokuapp.com/' + options.url);
@@ -37,4 +43,16 @@ else
         x.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
     }
     x.send(options.data);
-}       
+}
+
+/*
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    console.log(sender.tab ?
+                "from a content script:" + sender.tab.url :
+                "from the extension");
+    if (request.greeting == "hello")
+      sendResponse({farewell: "goodbye"});
+  });*/
+
+
